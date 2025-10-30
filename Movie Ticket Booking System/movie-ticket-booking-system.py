a6a504ea1479f2115db6c4fbd1c9eecfe94d63f0
Shashank Saurabh <shashanksaurabh91@gmail.com>
2025-10-30 23:40:45 +0530

    Movie ticket booking system LLD

---
+++ Movie Ticket Booking System/movie-ticket-booking-system.py
@@ -0,0 +1,11 @@
+# Requirements
+
+# 1. The system should allow users to view the list of movies playing in different theaters.
+# 2. Users should be able to select a movie, theater and show timing to book tickets
+# 3. The system should allow the seating arrangment of the selected show and allow users to choose seats.
+# 4. Users should be able to make payments and confirm thier booking.
+# 5. The system should handle concurrent bookings and ensure seat availability is updated in real time.
+# 6. The system should support different types of seats (e.g. normal, premium) and pricing.
+# 7. The system should allow theater admin to add, update and remove movies, show, and seating arrangements.
+# 8. Th e system should be scalable to handle a large number of concurrent users and bookings.
+
