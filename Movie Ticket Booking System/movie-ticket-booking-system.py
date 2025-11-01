c01d49671d7d054da9fa5388567509f145044c19
Shashank Saurabh <shashanksaurabh91@gmail.com>
2025-11-01 23:20:39 +0530

    Movie ticket booking system LLD

--- Movie Ticket Booking System/movie-ticket-booking-system.py
+++ Movie Ticket Booking System/movie-ticket-booking-system.py
@@ -10,6 +10,7 @@
 # 8. Th e system should be scalable to handle a large number of concurrent users and bookings.
 
 import uuid
+import time
 from enum import Enum
 from typing import List, Dict, Optional
 from abc import ABC, abstractmethod
@@ -208,7 +209,7 @@ class Payment:
 
 class PaymentStrategy(ABC):
     @abstractmethod
-    def pay(self, amount: float):
+    def pay(self, amount: float) -> Payment:
         pass
 
 class CreditCardPaymentStrategy(PaymentStrategy):
@@ -216,7 +217,7 @@ class CreditCardPaymentStrategy(PaymentStrategy):
         self.card_number = card_number
         self.cvv = cvv
 
-    def pay(self, amount: float):
+    def pay(self, amount: float) -> Payment:
         print(f"Processing credit card payment of ${amount:.2f}")
         # Simulate payment gateway interaction
         payment_success = random.random() > 0.05  # 95% success rate
@@ -225,3 +226,187 @@ class CreditCardPaymentStrategy(PaymentStrategy):
             PaymentStatus.SUCCESS if payment_success else PaymentStatus.FAILURE,
             f"TXN_{uuid.uuid4()}"
         )
+    
+class SeatLockManager:
+    def __init__(self):
+        self.locked_seats: Dict[Show, Dict[Seat, str]] = {}
+        self.executor = ThreadPoolExecutor(max_workers=5)
+        self.LOCK_TIMEOUT_SECONDS = 0.5
+
+    def lock_seats(self, show: Show, seats: List[Seat], user_id: str):
+        show_lock = getattr(show, '_lock', None)
+        if show_lock is None:
+            show._lock = threading.Lock()
+            show_lock = show._lock
+
+        with show_lock:
+            for seat in seats:
+                if seat.get_status() != SeatStatus.AVAILABLE:
+                    print(f"Seat {seat.get_id()} is not available.")
+                    return
+            
+            for seat in seats:
+                seat.set_status(SeatStatus.LOCKED)
+
+            if show not in self.locked_seats:
+                self.locked_seats[show] = {}
+
+            for seat in seats:
+                self.locked_seats[show[seat]] = user_id
+
+            self.executor.submit(self._unlock_after_timeout, show, seats, user_id)
+            print(f"Locked seats: {[seat.get_id() for seat in seats]} for user {user_id}")
+    
+    def _unlock_after_timeout(self, show: Show, seats: List[Seat], user_id: str):
+        time.sleep(self.LOCK_TIMEOUT_SECONDS)
+        self.unlock_seats(show, seats, user_id)
+
+    def unlock_seats(self, show: Show, seats: List[Seat], user_id: str):
+        show_lock = getattr(show, '_lock', None)
+        if show_lock is None:
+            return
+        
+        with show_lock:
+            show_locks = self.locked_seats.get(show)
+            if show_lock is not None:
+                for seat in seats:
+                    if seat in show_locks and show_locks[seat] == user_id:
+                        del show_lock[seat]
+                        if seat.get_status() == SeatStatus.LOCKED:
+                            seat.set_status(SeatStatus.AVAILABLE)
+                            print(f"Unlocked seat: {seat.get_id()} due to timeout.")
+                        else:
+                            print(f"Unlocked seat: {seat.get_id()} due to booking completion.")
+                if not show_locks:
+                    del self.lock_seats[show]
+
+    def shutdown(self):
+        print("Shutting down SeatLockProvider scheduler.")
+        self.executor.shutdown(wait=True)
+
+class Booking:
+    def __init__(self, booking_id: str, user: User, show: Show, seats: List[Seat], total_amount: float, payment: Payment):
+        self.id = booking_id
+        self.user = user
+        self.show = show
+        self.seats = seats
+        self.total_amount = total_amount
+        self.payment = payment
+
+    def confirm_booking(self):
+        for seat in self.seats:
+            seat.set_status(SeatStatus.BOOKED)
+    
+    def get_id(self):
+        return self.id
+    
+    def get_user(self) -> User:
+        return self.user
+
+    def get_show(self) -> Show:
+        return self.show
+
+    def get_seats(self) -> List[Seat]:
+        return self.seats
+
+    def get_total_amount(self) -> float:
+        return self.total_amount
+
+    def get_payment(self) -> Payment:
+        return self.payment
+    
+    class BookingBuilder:
+        def __init__(self):
+            self.id: Optional[str] = None
+            self.user: Optional[User] = None
+            self.show: Optional[Show] = None
+            self.seats: Optional[List[Seat]] = None
+            self.total_amount: Optional[float] = None
+            self.payment: Optional[Payment] = None
+
+        def set_id(self, booking_id: str) -> 'Booking.BookingBuilder':
+            self.id = booking_id
+            return self
+
+        def set_user(self, user: User) -> 'Booking.BookingBuilder':
+            self.user = user
+            return self
+
+        def set_show(self, show: Show) -> 'Booking.BookingBuilder':
+            self.show = show
+            return self
+
+        def set_seats(self, seats: List[Seat]) -> 'Booking.BookingBuilder':
+            self.seats = seats
+            return self
+
+        def set_total_amount(self, total_amount: float) -> 'Booking.BookingBuilder':
+            self.total_amount = total_amount
+            return self
+
+        def set_payment(self, payment: Payment) -> 'Booking.BookingBuilder':
+            self.payment = payment
+            return self
+        
+        def build(self) -> 'Booking':
+            return Booking(self.id, self.user, self.show, self.seats, self.total_amount, self.payment)
+        
+class BookingManager:
+    def __init__(self, seat_lock_manager: SeatLockManager):
+        self.seat_lock_manager = seat_lock_manager
+
+    def create_booking(self, user: User, show: Show, seats: List[Seat], payment_strategy: PaymentStrategy):
+        self.seat_lock_manager.lock_seats(show, seats, user.get_id())
+        total_amount = show.get_pricing_strategy().calculate_price(seats)
+        payment = payment_strategy.pay(total_amount)
+        if payment.get_status() == PaymentStatus.SUCCESS:
+            booking = Booking.BookingBuilder() \
+            .set_user(user) \
+            .set_show(show) \
+            .set_seats(seats) \
+            .set_total_amount(total_amount) \
+            .set_payment(payment) \
+            .build()
+
+            booking.confirm_booking()
+            self.seat_lock_manager.unlock_seats(show, seats, user.get_id())
+            return booking
+        else:
+            print("Payment failed. Please try again.")
+            return None
+        
+class MovieBookingService:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        if hasattr(self, 'Initialized'):
+            return
+        self.cities: Dict[str, City] = {}
+        self.cinemas: Dict[str, Cinema] = {}
+        self.movies: Dict[str, Movie] = {}
+        self.users: Dict[str, User] = {}
+        self.show: Dict[str, Show] = {}
+
+        self.seat_lock_manager = SeatLockManager()
+        self.booking_manager = BookingManager(self.seat_lock_manager)
+        self.initialized = True
+
+    @classmethod
+    def get_instance(cls):
+        return cls()
+    
+    def get_booking_manager(self):
+        return self.booking_manager
+    
+    def add_city(self, city_id: str, name: str):
+        city = City(city_id, name)
+        self.cities[city.get_id()] = city
+        return city
